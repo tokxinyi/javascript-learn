5059d81582473c7498e45f50d2becdd1e1531aa7
Xinyi <tokxinyi@gmail.com>
2021-10-14 16:47:32 +0800

    Added the code to count the number of vowels

--- vowel-checker/script.js
+++ vowel-checker/script.js
@@ -1,27 +1,20 @@
+const word = document.querySelector(".input-text");
 const btn = document.querySelector(".btn");
 const result = document.querySelector(".results");
 
-btn.addEventListener("click", palindrome);
 
-function palindrome() {
-    const word = document.querySelector(".input-text").value;
-    let length = word.length;
+btn.addEventListener("click", countVowel);
 
-    let start = word.substring(0, Math.floor(length/2)).toLowerCase();
-    // alert(start);
-    
-    let end = word.substring(length - Math.floor(length/2)).toLowerCase();
+function countVowel() {
+    let vowelCount = 0;
+    let wordVal = word.value.toLowerCase();
 
-    let flip = end.split("").reverse().join("");
-    // ES6 syntax
-    // let flip = [...end].reverse().join();
-    // alert(flip);
-
-    if (start == flip) {
-        result.innerHTML = `${word.toUpperCase()} is a palindrome`;
-    }
-    else {
-        result.innerHTML = `${word.toUpperCase()} is not a palindrome`;
+    for (let i = 0; i < wordVal.length; i++) {
+        let letter = wordVal[i];
+        if (letter.match(/[a,e,i,o,u]/)) {
+            vowelCount++;
+        }
     }
 
+    result.innerHTML = `${word.value.toUpperCase()} has ${vowelCount} vowels.`;
 }
